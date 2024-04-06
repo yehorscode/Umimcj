f4db8d21fe3b1fba1c7a9c0b21f0475a276baaad
Yehor <omelianenkoegor@gmail.com>
2024-04-06 15:22:15 +0200

    Created file

---
+++ index.py
@@ -0,0 +1,7 @@
+import requests
+from colorama import Fore, Back, Style
+import json
+
+# This is the app that wants to scan websites like: Modrinth, Curseforge
+
+headers = "https://github.com/yehorscode/CreateAddons-Site (create-addons@proton.me)"
